903e6d060d256f680d73faea364a1f1d981464ac
Arthurnucada <arthurnucada280204@gmail.com>
2024-04-19 16:16:53 -0300

    Criação classe Endereco

---
+++ endereco/Endereco.java
@@ -0,0 +1,11 @@
+package com.github.arthurnucada;
+import java.util.list;
+
+public class Endereço {
+    int numero;
+    String complemento;
+    int CEP;
+    List <tiposDeEndereco> tiposDeEndereco = new ArrayList();
+    Logadouro logad;
+    PessoaFisica pes;
+}
